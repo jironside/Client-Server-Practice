5f10801b08824ea1dc31aadd4aa1d2e36bb73d0a
Jamie Ironside <jamierironside@gmail.com>
2014-07-13 20:44:18 +0100

    First commit from the command line

---
+++ src/main/server/Starter.java
@@ -0,0 +1,9 @@
+package main.server;
+
+public class Starter {
+
+	//Hello world test class for git
+	public static void main(String args[]){
+		System.out.println("Hello World can you all see this?");
+	}
+}
